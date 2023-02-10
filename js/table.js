5b90ba0741ca55322b48f998a0517776798f660f
ryutarote <75284855+ryutarote@users.noreply.github.com>
2023-02-10 15:31:19 -0800

    Add a feature to download csv file

--- js/table.js
+++ js/table.js
@@ -1,117 +1,135 @@
- /**
-  * Using es6 template strings for data.
-  *
-  */
+/**
+ * Using es6 template strings for data.
+ *
+ */
 
-export default class  {
-  /**
-   * @param tableElm This will display the CSV data on the table.
-   */
-  constructor(tableElm) {
-    this.table = tableElm;
-  }
+export default class {
+	/**
+	 * @param tableElm This will display the CSV data on the table.
+	 */
+	constructor(tableElm) {
+		this.table = tableElm;
+	}
 
-  /**
-   * Clears previous data (In case user uploads a new file right after), and imports the new
-   * data.
-   *
-   * @param data 2D Array of data.
-   * @param headerC Headers Column.
-   */
+	/**
+	 * Clears previous data (In case user uploads a new file right after), and imports the new
+	 * data.
+	 *
+	 * @param data 2D Array of data.
+	 * @param headerC Headers Column.
+	 */
 
+	updateList(data, headerC) {
+		let common = ['Representatives'];
 
-  updateList(data, headerC){
-    let common = ["Representatives"];
+		this.removeList();
+		this.createHead(headerC, common);
+		this.importData(data);
+	}
 
+	/**
+	 * This method clears all data and header.
+	 */
+	removeList() {
+		this.table.innerHTML = '';
+	}
 
-    this.removeList();
-    this.createHead(headerC, common);
-    this.importData(data);
+	/**
+	 * Sets the table headers
+	 * @param headerC
+	 * @param common Array of Strings for the Divide, Floor and Remain.
+	 */
 
-  }
-
-  /**
-   * This method clears all data and header.
-   */
-  removeList() {
-    this.table.innerHTML = "";
-  }
-
-  /**
-   * Sets the table headers
-   * @param headerC
-   * @param common Array of Strings for the Divide, Floor and Remain.
-   */
-
-  createHead(headerC, common){
-    this.table.insertAdjacentHTML("afterbegin", `
+	createHead(headerC, common) {
+		this.table.insertAdjacentHTML(
+			'afterbegin',
+			`
         <thead>
-            ${ headerC.map(text => `<th>${text}</th>`).join("") }
-            ${common.map(cmnText => `<th>${cmnText}</th>`).join("") }
+            ${headerC.map((text) => `<th>${text}</th>`).join('')}
+            ${common.map((cmnText) => `<th>${cmnText}</th>`).join('')}
         </thead>
-    `);
-  }
+    `
+		);
+	}
 
-  /**
-   * Imports the data into the table body by using a 2D array of type string and calculates the number of representatives
-   * per state.
-   * @param data
-   */
+	/**
+	 * Imports the data into the table body by using a 2D array of type string and calculates the number of representatives
+	 * per state.
+	 * @param data
+	 */
 
-  importData(data){
-    const tableRow = [];
-    let totalPop = 0;
-    for(let i = 0; i < data.length; i++) {
-      totalPop += parseInt(data[i][1]);
-    }
+	importData(data) {
+		const tableRow = [];
+		let totalPop = 0;
+		for (let i = 0; i < data.length; i++) {
+			totalPop += parseInt(data[i][1]);
+		}
 
-    let numOfRep = document.getElementById("numOfRep").value;
-    let numOfRepVal = parseInt(numOfRep);
+		let numOfRep = document.getElementById('numOfRep').value;
+		let numOfRepVal = parseInt(numOfRep);
 
-    let avgPopPerRep = totalPop/numOfRepVal;
+		let avgPopPerRep = totalPop / numOfRepVal;
 
-    for(let j = 0; j < data.length; j++) {
-      let totalNumOfRep = data[j][1]/avgPopPerRep;
-      data[j].push(Math.floor(totalNumOfRep));
-    }
-    
-    let finalNum = 0;
-    for(let i = 0; i < data.length; i++) {
-      finalNum += data[i][2];
-    }
+		for (let j = 0; j < data.length; j++) {
+			let totalNumOfRep = data[j][1] / avgPopPerRep;
+			data[j].push(Math.floor(totalNumOfRep));
+		}
 
-    let remainderArray = [];
+		let finalNum = 0;
+		for (let i = 0; i < data.length; i++) {
+			finalNum += data[i][2];
+		}
 
-    for(let x = 1; x < data.length; x++) {
-      remainderArray.push(data[x][1]%avgPopPerRep);
-    }
+		let remainderArray = [];
 
-    let missingReps = numOfRepVal - finalNum;
-    let max = 0;
-    let maxIndex = 0;
-    for(let y = 0; y < missingReps; y++) {
-      for(let e = 1; e < data.length; e++) {
-        if(remainderArray[e] > max) {
-          maxIndex = e;
-          max = remainderArray[e];
-        }
-      }
-      data[maxIndex][2]++;
-    }
-    data.sort();
-    for (let row of data) {
-      tableRow.push(`
+		for (let x = 1; x < data.length; x++) {
+			remainderArray.push(data[x][1] % avgPopPerRep);
+		}
+
+		let missingReps = numOfRepVal - finalNum;
+		let max = 0;
+		let maxIndex = 0;
+		for (let y = 0; y < missingReps; y++) {
+			for (let e = 1; e < data.length; e++) {
+				if (remainderArray[e] > max) {
+					maxIndex = e;
+					max = remainderArray[e];
+				}
+			}
+			data[maxIndex][2]++;
+		}
+		data.sort();
+		for (let row of data) {
+			tableRow.push(`
             <tr>
-                ${row.map(text => `<td> ${text} </td>`).join("")}
+                ${row.map((text) => `<td> ${text} </td>`).join('')}
 
             </tr>
                `);
-    }
-    this.table.insertAdjacentHTML("beforeend", `
+		}
+		this.table.insertAdjacentHTML(
+			'beforeend',
+			`
         <tbody>
-            ${ tableRow.join("") }
+            ${tableRow.join('')}
         </tbody>
-      `);
-    // console.log(parseInt(data[0][1]) + parseInt(data[1][1]));
-  }
+      `
+		);
+
+		// Define the content of the CSV file as a string
+		let csvContent = 'data:text/csv;charset=utf-8,';
+
+		// Loop through each row of data
+		data.forEach(function (rowArray) {
+			// Join the elements of the row array into a string
+			let data = rowArray.join(',');
+			// Add the row string to the overall CSV content
+			csvContent += data + '\r\n';
+		});
+
+		// Encode the CSV content as a URI
+		let encodedUri = encodeURI(csvContent);
+		// Open the encoded URI in a new window
+		window.open(encodedUri);
+	}
 }
