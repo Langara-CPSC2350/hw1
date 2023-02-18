881d68f8c69dc5dd784700170c8fbd330d6aed25
Frayan <fforoughi00@mylangara.ca>
2023-02-17 20:20:08 -0800

    Final Javascript edits: - Fixed the error for error detection that if number of representatives is lower than states that it should end the program

--- js/table.js
+++ js/table.js
@@ -3,13 +3,9 @@
  *
  */
 let userChoice;
-let error = false;
-let numOfRep = document.getElementById("numOfRep").value;
-let numOfRepVal = parseInt(numOfRep);
 let totalStates = 0;
 function pick(choice) {
 	userChoice = choice;
-	console.log("This is userchoice" + userChoice);
 }
 
 let button1 = document.getElementById("hamilton");
@@ -79,109 +75,110 @@ export default class {
 	 */
 
 	importData(data) {
-		const tableRow = [];
-		if(userChoice === 0) {
-			//this means that they are going with huntington algorithm
-			for(let i = 0; i < data.length; i++) {
-				totalStates++;
-			}
-			if(numOfRepVal < totalStates) {
-				alert("error: not enough reps, please try again.");
-				error = true;
-			} else {
-				let count = 0;
-				let priorityArray = [];
-				for(let i = 0; i < data.length; i++) {
-					data[i].push(1);
-					priorityArray.push(data[i][1]/Math.sqrt((data[i][2] * (data[i][2] + 1))));
-					count++;
-				}
-				for(let i = 0; i < (numOfRep - count); i++) {
-					let maxIndex = 0;
-					let max = 0;
-					for(let j = 0; j < priorityArray.length; j++) {
-						if (max < priorityArray[j]) {
-							maxIndex = j;
-							max = priorityArray[j];
+		let numOfRep = document.getElementById("numOfRep").value;
+		let numOfRepVal = parseInt(numOfRep);
+		for (let i = 0; i < data.length; i++) {
+			totalStates++;
+		}
+		if (numOfRepVal < totalStates) {
+			alert("error: Not enough representatives, please try again");
+		} else {
+			const tableRow = [];
+			if (userChoice === 0) {
+				//this means that they are going with huntington algorithm
+					let count = 0;
+					let priorityArray = [];
+					for (let i = 0; i < data.length; i++) {
+						data[i].push(1);
+						priorityArray.push(data[i][1] / Math.sqrt((data[i][2] * (data[i][2] + 1))));
+						count++;
+					}
+					for (let i = 0; i < (numOfRep - count); i++) {
+						let maxIndex = 0;
+						let max = 0;
+						for (let j = 0; j < priorityArray.length; j++) {
+							if (max < priorityArray[j]) {
+								maxIndex = j;
+								max = priorityArray[j];
+							}
+						}
+						data[maxIndex][2]++
+						priorityArray = [];
+						for (let i = 0; i < data.length; i++) {
+							priorityArray.push(data[i][1] / Math.sqrt((data[i][2] * (data[i][2] + 1))));
 						}
 					}
-					data[maxIndex][2]++
-					priorityArray = [];
-					for(let i = 0; i < data.length; i++) {
-						priorityArray.push(data[i][1]/Math.sqrt((data[i][2] * (data[i][2] + 1))));
+			} else {
+				// this means they are going with hamiliton algorithm
+				let totalPop = 0;
+				for (let i = 0; i < data.length; i++) {
+					totalPop += parseInt(data[i][1]);
+				}
+
+				let avgPopPerRep = totalPop / numOfRepVal;
+
+				for (let j = 0; j < data.length; j++) {
+					let totalNumOfRep = data[j][1] / avgPopPerRep;
+					data[j].push(Math.floor(totalNumOfRep));
+				}
+
+				let finalNum = 0;
+				for (let i = 0; i < data.length; i++) {
+					finalNum += data[i][2];
+				}
+
+				let remainderArray = [];
+
+				for (let x = 1; x < data.length; x++) {
+					remainderArray.push(data[x][1] % avgPopPerRep);
+				}
+
+				let missingReps = numOfRepVal - finalNum;
+				let max = 0;
+				let maxIndex = 0;
+				for (let y = 0; y < missingReps; y++) {
+					for (let e = 1; e < data.length; e++) {
+						if (remainderArray[e] > max) {
+							maxIndex = e;
+							max = remainderArray[e];
+						}
 					}
+					data[maxIndex][2]++;
 				}
 			}
-		} else {
-			// this means they are going with hamiliton algorithm
-			let totalPop = 0;
-			for(let i = 0; i < data.length; i++) {
-				totalPop += parseInt(data[i][1]);
-			}
-
-			let avgPopPerRep = totalPop/numOfRepVal;
-
-			for(let j = 0; j < data.length; j++) {
-				let totalNumOfRep = data[j][1]/avgPopPerRep;
-				data[j].push(Math.floor(totalNumOfRep));
-			}
-
-			let finalNum = 0;
-			for(let i = 0; i < data.length; i++) {
-				finalNum += data[i][2];
-			}
-
-			let remainderArray = [];
-
-			for(let x = 1; x < data.length; x++) {
-				remainderArray.push(data[x][1]%avgPopPerRep);
-			}
-
-			let missingReps = numOfRepVal - finalNum;
-			let max = 0;
-			let maxIndex = 0;
-			for(let y = 0; y < missingReps; y++) {
-				for(let e = 1; e < data.length; e++) {
-					if(remainderArray[e] > max) {
-						maxIndex = e;
-						max = remainderArray[e];
-					}
-				}
-				data[maxIndex][2]++;
-			}
-		}
-		data.sort();
-		for (let row of data) {
-			tableRow.push(`
+			data.sort();
+			for (let row of data) {
+				tableRow.push(`
             <tr>
                 ${row.map((text) => `<td> ${text} </td>`).join('')}
 
             </tr>
                `);
-		}
-		this.table.insertAdjacentHTML(
-			'beforeend',
-			`
+			}
+			this.table.insertAdjacentHTML(
+				'beforeend',
+				`
         <tbody>
             ${tableRow.join('')}
         </tbody>
       `
-		);
+			);
 
-		// Define the content of the CSV file as a string
-		let csvContent = 'data:text/csv;charset=utf-8,';
+			// Define the content of the CSV file as a string
+			let csvContent = 'data:text/csv;charset=utf-8,';
 
-		// Loop through each row of data
-		data.forEach(function (rowArray) {
-			// Join the elements of the row array into a string
-			let data = rowArray.join(',');
-			// Add the row string to the overall CSV content
-			csvContent += data + '\r\n';
-		});
+			// Loop through each row of data
+			data.forEach(function (rowArray) {
+				// Join the elements of the row array into a string
+				let data = rowArray.join(',');
+				// Add the row string to the overall CSV content
+				csvContent += data + '\r\n';
+			});
 
-		// Encode the CSV content as a URI
-		let encodedUri = encodeURI(csvContent);
-		// Open the encoded URI in a new window
-		window.open(encodedUri);
+			// Encode the CSV content as a URI
+			let encodedUri = encodeURI(csvContent);
+			// Open the encoded URI in a new window
+			window.open(encodedUri);
+		}
 	}
 }
